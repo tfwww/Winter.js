ce9c326c46250bd12ea180667e0f0ff432febf33
Winter Zhong <zhongwm.net@gmail.com>
2017-04-21 21:15:58 +0800

    同步

--- pager.js
+++ pager.js
@@ -1,5 +1,6 @@
 // 假设后端传的数据是这样的
-var data = [1, 2, 3, 4, 5, 6]
+var data = [1, 2, 3, 4, 5, 6, 7, 'a', 'b']
+var dataArray = sourceData.data.list
 
 var winterPage = {}
 var log = function() {
@@ -31,14 +32,33 @@ var insertTpl = function(selStr, tpl) {
 
 var insertBtns = function(data) {
     for (let i = 0; i < data.length; i++) {
-        insertTpl('.pager', btnTpl(data[i]))
+        insertTpl('.pager', btnTpl(i + 1))
     }
 }
 
+function Page(dataArray) {
+    var o = new Object()
+    // 一页显示 10 行
+    o.row = 10
+    this.btnContainer = function() {
+        var bodyEle = document.querySelector('body')
+        bodyEle.insertAdjacentHTML('beforeEnd', '<div class="pager"></div>')
+    }
+
+    o.makePageBtns = function(){
+        insertBtns(data)
+        for (var i = 0; i < pages.length; i++) {
+            insertTpl('.pager', btnTpl(pages[i]))
+        }
+    };
+    return o;
+}
+
 var __main = function() {
-    btnContainer()
-    insertBtns(data)
-    bindClickEvent()
+    var page = new Page()
+    page.btnContainer()
+    // bindClickEvent()
+    log('sourceData', sourceData.data.list)
 }
 
 __main()
