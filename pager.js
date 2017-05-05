b813b04029d5674a67ac3b3f71fa27fb7122fa74
Winter Zhong <zhongwm.net@gmail.com>
2017-05-05 20:05:23 +0800

    同步

--- pager.js
+++ pager.js
@@ -1,65 +1,63 @@
-// 假设后端传的数据是这样的
-var data = [1, 2, 3, 4, 5, 6, 7, 'a', 'b']
-var dataArray = sourceData.data.list
+//测试数据
+var data = [
+    '北京',
+    '上海',
+    '广州',
+    '深圳',
+    '杭州',
+    '长沙',
+    '合肥',
+    '宁夏',
+    '成都',
+    '西安',
+    '南昌',
+    '上饶',
+    '沈阳',
+    '济南',
+    '厦门',
+    '福州',
+    '九江',
+    '宜春',
+    '赣州',
+    '宁波',
+    '绍兴',
+    '无锡',
+    '苏州',
+    '徐州',
+    '东莞',
+    '佛山',
+    '中山',
+    '成都',
+    '武汉',
+    '青岛',
+    '天津',
+    '重庆',
+    '南京',
+    '九江',
+    '香港',
+    '澳门',
+    '台北'
+];
 
-var winterPage = {}
-var log = function() {
-    console.log.apply(console, arguments)
-}
+var nums = 5; //每页出现的数量
+var pages = Math.ceil(data.length/nums); //得到总页数
 
-// 监听点击按钮获取不同页面的数据
-var bindClickEvent =function() {
-    var pagerDivEle = document.querySelector('.pager')
-    pagerDivEle.addEventListener('click', function(event) {
-        log('click target', event.target)
-    })
-}
-
-var btnContainer = function() {
-    var bodyEle = document.querySelector('body')
-    bodyEle.insertAdjacentHTML('beforeEnd', '<div class="pager"></div>')
-}
-
-var btnTpl = function(inx) {
-    var t = `<button type="button" name="button">${inx}</button>`
-    return t
-}
-
-var insertTpl = function(selStr, tpl) {
-    var ele = document.querySelector(selStr)
-    ele.insertAdjacentHTML('beforeEnd', tpl)
-}
-
-var insertBtns = function(data) {
-    for (let i = 0; i < data.length; i++) {
-        insertTpl('.pager', btnTpl(i + 1))
+var thisDate = function(curr){
+    //此处只是演示，实际场景通常是返回已经当前页已经分组好的数据
+    var str = '', last = curr*nums - 1;
+    last = last >= data.length ? (data.length-1) : last;
+    for(var i = (curr*nums - nums); i <= last; i++){
+        str += '<li>'+ data[i] +'</li>';
     }
-}
+    return str;
+};
 
-function Page(dataArray) {
+//调用分页
+laypage({
+    cont: 'biuuu_city',
+    pages: pages,
+    jump: function(obj){
+        document.getElementById('biuuu_city_list').innerHTML = thisDate(obj.curr);
 
-}
-
-Page.prototype = {
-    constructor: Page,
-    btnContainer: function() {
-        var bodyEle = document.querySelector('body')
-        bodyEle.insertAdjacentHTML('beforeEnd', '<div class="pager"></div>')
-    },
-    makePageBtns: function() {
-        for (var i = 0; i < data.length; i++) {
-            insertTpl('.pager', btnTpl(i + 1))
-        }
     }
-}
-
-var __main = function() {
-    var page = new Page()
-    page.btnContainer()
-    page.makePageBtns(data)
-    // bindClickEvent()
-    log('page', page)
-    log('sourceData', sourceData.data.list)
-}
-
-__main()
+})
